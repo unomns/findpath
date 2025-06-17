2f52c07337752c56ea8ab57533ea1622562e6210
unomns <unomns@gmail.com>
2025-06-17 13:36:59 +0800

    grpc p2. graceful shutdown

--- cmd/findpath-grpc/main.go
+++ cmd/findpath-grpc/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 	app_grpc "unomns/findpath/internal/grpc"
 	findpathv1 "unomns/findpath/protos/gen/findpath"
 
@@ -10,20 +15,35 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
+	port := flag.String("port", "50051", "Port for gRPC server")
+	flag.Parse()
+
+	addr := fmt.Sprintf(":%s", *port)
+
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to listen on %s: %v", addr, err)
 	}
 
 	grpcServer := grpc.NewServer()
-
 	findpathv1.RegisterPathFinderServer(
 		grpcServer,
 		app_grpc.NewServer(),
 	)
 
-	log.Println("gRPC server listening on :50051")
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	go func() {
+		log.Printf("gRPC server listening on %s\n", addr)
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	<-stop
+
+	log.Println("Shutting down gRPC server gracefully...")
+	grpcServer.GracefulStop()
+	log.Println("gRPC server stopped")
 }
